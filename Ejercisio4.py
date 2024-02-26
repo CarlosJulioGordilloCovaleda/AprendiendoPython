48e22b9f69eec5d3d39401bc5c14bdac60f7e910
CarlosJulioGordilloCovaleda <160853185+CarlosJulioGordilloCovaleda@users.noreply.github.com>
2024-02-25 19:59:20 -0500

    Ejercisio de Grados

---
+++ Ejercisio4.py
@@ -0,0 +1,5 @@
+#Algortimo para conversitr grados Celcius a Farenheit
+
+GradosC=float(input("Ingrese los grados Celcius a convertir: "))
+GradosF=float(GradosC*1.8)+32
+print(GradosC, " Grados celcius son ", GradosF, "Grados Farehenit")
