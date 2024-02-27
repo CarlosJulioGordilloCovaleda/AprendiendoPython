5627b7c49fc94f4aa05e061d67abc847a7102777
CarlosJulioGordilloCovaleda <160853185+CarlosJulioGordilloCovaleda@users.noreply.github.com>
2024-02-27 08:51:54 -0500

    Add files via upload
    Ejercisios para el taller

---
+++ ejercisio.py
@@ -0,0 +1,19 @@
+# hacer un menu y cada menu debe tener una categoria por ejemplo si hago una categoria de postres debe salir postre de limon, roscones,pastel de 3 leches despues de seleccionar los productos
+# uno debe seleccionar la cantidad de producto que quiera y al final que compruebe precios ventas y todo lo del anteior ejemplo
+# 3 Categorias 10 productos por categorias y 2 promiciones po categoria
+
+# 3 Categorias panaderia basica,postres,gourmet 
+Menu=list(
+    productos_panaderia = {
+    'Pan blanco': 800,
+    'Pan integral': 1000,
+    'Bollo de pan': 600,
+    'Croissant': 2300,
+    'Pan de sal': 600,
+    'Bagel': 1500,
+    'Pan de dulce': 500,
+    'Tamal': 5500,
+    'Embuelto': 3800,
+    'Empanada de carne': 2300
+
+
