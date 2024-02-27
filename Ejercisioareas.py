af5189c1a3cacdee9606801ce39d0ce614e53fa1
CarlosJulioGordilloCovaleda <160853185+CarlosJulioGordilloCovaleda@users.noreply.github.com>
2024-02-26 20:47:49 -0500

    Area de un triangullo

---
+++ Ejercisioareas.py
@@ -0,0 +1,8 @@
+# Algortimo para calcular el area de un triangulo
+print("Tenga en cuenta que todas las unidades ingresadas deben ser en la misma unidad de medida ( cm,m,ft,in)")
+base=float(input("Ingrese la base del triangulo: "))
+altura=float(input("Ingrese la altura del triangulo: "))
+
+Area=(base*altura)/2
+
+print(" El area del triangulo es : ",Area)
