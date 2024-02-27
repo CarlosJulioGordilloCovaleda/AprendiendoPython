7854d84d0c18deefe5898522c6bae15c71812a8a
CarlosJulioGordilloCovaleda <160853185+CarlosJulioGordilloCovaleda@users.noreply.github.com>
2024-02-26 21:18:39 -0500

    Este ejercisio es para sacar areas de un triangulo

--- Ejercisioareas.py
+++ Ejercisioareas.py
@@ -1,8 +1,16 @@
 # Algortimo para calcular el area de un triangulo
+import math
 print("Tenga en cuenta que todas las unidades ingresadas deben ser en la misma unidad de medida ( cm,m,ft,in)")
-base=float(input("Ingrese la base del triangulo: "))
-altura=float(input("Ingrese la altura del triangulo: "))
 
-Area=(base*altura)/2
+opcion=input("Si desea realizar el calulo con el radio digite(r) y si lo desea con el diametro digite (d) : ")
 
-print(" El area del triangulo es : ",Area)
+if opcion.lower()=='r':
+    radio =float(input("Ingrese el radio del circulo : "))
+    Area=math.pi * radio**2
+    print("El area del cirulo es : ", Area)
+elif opcion.lower()=='d':
+    diametro=float(input("Ingrese el diametro del circulo : "))
+    Area=( math.pi * diametro**2 ) / 4
+    print("El area del circulo es : ", Area)
+else:
+    print("Opcion no valida por favor ingrese  (r)para radio o (d)diametro")
