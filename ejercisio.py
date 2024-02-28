a50e7b6eabdd3d772966456ce5284bd26283382e
CarlosJulioGordilloCovaleda <160853185+CarlosJulioGordilloCovaleda@users.noreply.github.com>
2024-02-27 20:51:02 -0500

    Primera categoria del Ejercisio Panaderia

--- ejercisio.py
+++ ejercisio.py
@@ -9,15 +9,30 @@ Menu = {
         {"nombre": "Pan de Sal", "Precio": 1000},
         {"nombre": "Croasan", "Precio": 1500},
         {"nombre": "Empanadas de pollo", "Precio": 1800},
-        {"nombre": "Empanadas de queso", "Precio": 0.6},
-        {"nombre": "Tamales", "Precio": 0.6},
-        {"nombre": "Embuelto", "Precio": 0.6},
-        {"nombre": "Tamal Tolimense", "Precio": 0.6},
-        {"nombre": "Panuchas", "Precio": 0.6},
-        {"nombre": "Galletas", "Precio": 0.6},
+        {"nombre": "Empanadas de queso", "Precio": 1300},
+        {"nombre": "Tamales", "Precio": 1700},
+        {"nombre": "Embuelto", "Precio": 1800},
+        {"nombre": "Tamal Tolimense", "Precio": 5000},
+        {"nombre": "Panuchas", "Precio": 2000},
+        {"nombre": "Galletas", "Precio": 2300},
     ]        
 }
-for i,val in enumerate("nombre"): 
-    print(f"""{i}, {val}, ${"precios"[i]} """)
 
-print(Menu)
+print ("                                        Menu                                        ")
+for i in Menu.keys(): 
+    print(" - ",i)
+for indice,producto in enumerate(Menu["Tradicionales"],start=1):
+    print(" - {}. {} : $ {}".format(indice, producto["nombre"], producto["Precio"]))
+
+opcion=int(input("Seleccione el producto que desea comprar : "))
+producto_seleccionado = Menu["Tradicionales"][opcion - 1]
+print(f"El producto que usted escogio es {producto_seleccionado['nombre']} con un valor de  $ {producto_seleccionado['Precio']}")
+dinero=int(input("El valor del dinero disponible $ : "))
+vueltos=dinero-producto_seleccionado["Precio"]
+if dinero>=opcion:
+    print(f"Gracias por su compra, su regreso es de $  {vueltos}")
+else:
+    print(f"El dinero depositado no es suficiente le falta un total de $ {-vueltos}")
+
+
+
