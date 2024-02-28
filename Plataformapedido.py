7e0ef0f099533349479911b149b1d52b9824c722
Dianaalejandra1446 <dianaleja04@hotmail.com>
2024-02-28 13:24:52 -0500

    Plataformadeprueba

---
+++ Plataformapedido.py
@@ -0,0 +1,9 @@
+# Primero intento para hacer una plataforma que tome pedidos de una empresa de alimentos, esta plataforma es para un vendedor
+# Cuando realiza ventas la plataforma le da el valor del pedido puede seleccionar cliente y los productos que este solicita
+# Da un % de descuento si cliente sobrepasa un monto y tambien voy a tratar de dar un porcentaje al vendedor sobre la venta que lo calcule
+# Productos : Platanitos Salados,Maiz Molido,Frijo lima,Mantequilla de Mani,Mani salado
+
+clientes=("Mas o menos","HipermercadosExito","Supermercados Metro","Agromercados de la montaña")
+
+print(clientes)
+
