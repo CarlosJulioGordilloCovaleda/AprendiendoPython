a2361719bd6a7df35bd122e999b9ca35f141a3f4
CarlosJulioGordilloCovaleda <160853185+CarlosJulioGordilloCovaleda@users.noreply.github.com>
2024-02-29 15:29:04 -0500

    Paso 2

--- Plataformapedido.py
+++ Plataformapedido.py
@@ -4,6 +4,21 @@
 # Productos : Platanitos Salados,Maiz Molido,Frijo lima,Mantequilla de Mani,Mani salado
 
 clientes=("Mas o menos","HipermercadosExito","Supermercados Metro","Agromercados de la montaña")
+Productos=[
+            {"nombre":"Platanitos verdes","precio":1200},
+            {"nombre":"Platanitos verdes","precio":1200},
+            {"nombre":"Platanitos verdes","precio":1200},
+            {"nombre":"Platanitos verdes","precio":1200},
+            {"nombre":"Platanitos verdes","precio":1200},
+            {"nombre":"Platanitos verdes","precio":1200}
+]
+print("----- Clientes -----")
 
+for i,val in enumerate(clientes,start=1):
+    print(i,val)
+Clienteelegido=int(input("Ingrese el codigo del cliente"))
+
+
+print(Productos)
 print(clientes)
 
